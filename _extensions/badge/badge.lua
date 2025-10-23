a3d82d11d7bfd60f79329c00f2982108762e59f0
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2025-10-23 23:39:31 +0200

    refactor: simplify utils module loading

--- _extensions/badge/badge.lua
+++ _extensions/badge/badge.lua
@@ -23,8 +23,7 @@
 ]]
 
 --- Load utils module
-local utils_path = quarto.utils.resolve_path("_modules/utils.lua")
-local utils = require(utils_path)
+local utils = require(quarto.utils.resolve_path("_modules/utils.lua"):gsub("%.lua$", ""))
 
 --- Badge shortcode handler.
 --- Generates styled badge elements from document metadata configuration.
